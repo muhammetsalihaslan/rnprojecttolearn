1b4601c87424078aeb2fee16d01fcc706cdf8c2d
Your Name <you@example.com>
2024-03-19 12:17:36 +0300

    new

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from './pages/auth/Login';
+import Sign from './pages/auth/Sign';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const AuthStack = () => {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="LoginPage" component={Login} />
+        <Stack.Screen name="SignPage" component={Sign} />
+      </Stack.Navigator>
+    );
+  };
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen />
-        <Stack.Screen />
+        <Stack.Screen name="AuthStack" component={AuthStack} />
+        {/* <Stack.Screen name="" component={} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
