1cef22228426aac353bef79148e1da331b3071aa
Your Name <you@example.com>
2024-03-19 11:26:02 +0300

    realtime database

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -17,11 +17,37 @@ const App = () => {
     });
   };
 
+  const setDB = () => {
+    const reference = database().ref('car/rentable');
+    reference.set({
+      brand: 'Audi',
+      model: 'A8',
+      price: 128,
+    });
+  };
+  const updateDB = () => {
+    const reference = database().ref('car/rentable');
+    reference.update({
+      model: 'A3',
+    });
+  };
+  const pushDB = () => {
+    const reference = database().ref('car/rentable');
+    reference.push({
+      brand: 'Passat',
+      model: '81',
+      price: 650,
+    });
+  };
+
   return (
     <View>
       <Text style={{fontSize: 70}}>Hello fireBase</Text>
       <Button title="Check DB" onPress={checkDB} />
       <Button title="Listen DB" onPress={listenDB} />
+      <Button title="Set DB" onPress={setDB} />
+      <Button title="Update DB" onPress={updateDB} />
+      <Button title="Push DB" onPress={pushDB} />
     </View>
   );
 };
